8e1980089a5e0c092a8d6590bb51a745ffed6a34
Zeus Solomon Q. Olivar <zeusqolivar@icloud.com>
2023-10-17 23:47:36 +0800

    Second Commit

--- app/src/main/java/com/example/githubdemoapp_android/MainActivity.kt
+++ app/src/main/java/com/example/githubdemoapp_android/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Test Commit
+        //Test Commit 2nd
     }
 }
